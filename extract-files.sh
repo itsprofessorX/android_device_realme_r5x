49f043e58afd27a9a7f1692724a4deab09f6c4f7
AmritoRock <amritokarmokar5i@gmail.com>
2023-10-23 16:24:22 +0530

    r5x: Patch more blobs to load libprotobuf-cpp-full-3.9.1
    - Cmd grep -r libprotobuf-cpp-lite-3.9.1.so
    
    grep: proprietary/vendor/lib/libsnsapi.so: binary file matches
    grep: proprietary/vendor/lib/libsnsdiaglog.so: binary file matches
    grep: proprietary/vendor/lib64/mediadrm/libwvdrmengine.so: binary file matches
    grep: proprietary/vendor/lib64/libwvhidl.so: binary file matches
    grep: proprietary/vendor/lib64/libsnsapi.so: binary file matches
    
    Signed-off-by: Amritorock <amritokarmokar5i@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,15 @@ function blob_fixup() {
         vendor/lib64/libwvhidl.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             ;;
+       vendor/lib/libsnsdiaglog.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
+       vendor/lib64/libsnsapi.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
+       vendor/lib/libsnsapi.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
     esac
 }
 
