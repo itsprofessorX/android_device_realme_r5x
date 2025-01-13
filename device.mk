a70cf1cec732ee9fc7d74ed224700f93b1887e45
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    r5x: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -307,6 +307,12 @@ PRODUCT_PACKAGES += \
     BluetoothOverlayR5x \
     ApertureOverlay
 
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
